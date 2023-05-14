b15dac6db38d456f10795be5993efe062f76952b
GHYJune <ghyjune@outlook.com>
2023-05-14 21:05:41 +0800

    get index questions

--- src/main/java/ghy/community/Controller/IndexController.java
+++ src/main/java/ghy/community/Controller/IndexController.java
@@ -1,7 +1,11 @@
 package ghy.community.Controller;
 
+import ghy.community.dto.QuestionDTO;
+import ghy.community.mapper.QuestionMapper;
 import ghy.community.mapper.UserMapper;
+import ghy.community.model.Question;
 import ghy.community.model.User;
+import ghy.community.service.QuestionService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +14,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model){
         Cookie[] cookies = request.getCookies();
         if(cookies!=null)
         {
@@ -32,6 +41,9 @@ public class IndexController {
                 }
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
